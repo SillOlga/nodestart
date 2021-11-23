2e7fa0dbe9f449c2d5508db163c95ee28e7d293e
SillOlga <Olga.Ivanova.sill@gmail.com>
2021-11-23 13:09:11 +0200

    File webserver.js izmenenije

--- webserver/webserver.js
+++ webserver/webserver.js
@@ -1,21 +1,30 @@
-const http = require('http')
+http = require('http')
 
-const server = http.createServer((req, res) => {
+path = require('path');
+
+server = http.createServer((req, res) => {
     console.log(req.url)
 
     res.write('<h1>Hello from SKTVp20!</h1>')
     res.write('<h2>Sergey is a wonderful teacher!</h2>')
-    res.end(`
-        <div style="background: orange; width; 100px; height: 100px;">
-            <img src="webserver/img/ivkhk.jpg" width="400" height="300">
-            <hr>
-            <h3>Ida-Virumaa Kutsehariduskeskus 2021</h3>
-        </div>
-    `)
-})
+    res.write('<h3>Ida-Virumaa Kutsehariduskeskus 2021</h3')
+    res.write('<img style="background: orange; width; 100px; height: 100px;" src="'  + path.dirname(__filename) + "/ivkhk.jpg" + '">')
 
-server.listen(3002, () => {
+    res.write('</div>')
+
+    res.write('</div>')
+
+    res.end()
+
+
+ })
+
+
+ server.listen(3001, () => {
+
     console.log('Server is running...')
-})
 
-
+ })
+ 
+ 
+ 
