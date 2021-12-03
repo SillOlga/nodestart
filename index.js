58418f65c56dada9b1ab4282c4f113013efa23f1
SillOlga <Olga.Ivanova.sill@gmail.com>
2021-12-03 19:20:04 +0200

    Nodestart update

--- index.js
+++ index.js
@@ -1,67 +1,87 @@
-// The HTTP interfaces in Node.js are designed to support many features of the protocol which have been traditionally difficult to use.
-const http = require('http')
-// The path module provides utilities for working with file and directory paths. It can be accessed using
+const http = require('http')  // To use the HTTP server
 const path = require('path')
-// The fs module enables interacting with the file system in a way modeled on standard POSIX functions.
 const fs = require('fs')
 
-// The requestListener is a function which is automatically added to the 'request' event.
+// Create a local server to receive data from
 const server = http.createServer((req, res) => {
-  if (req.method === 'GET') {
-    // Returns a reference to the ServerResponse, so that calls can be chained.
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
-    })
+    if (req.method === 'GET') {
+        // Sends a response header to the request. The status code is a 3-digit HTTP status code, like 404.
+        // The last argument, headers, are the response headers.
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        })
 
-    if (req.url === '/') {
-      // Asynchronously reads the entire contents of a file.
-      fs.readFile(
-        path.join(__dirname, 'views', 'index.html'),
-        'utf-8',
-        (err, content) => {
-          if (err) {
-            throw err
-          }
+        if (req.url === '/') {
+            fs.readFile(
+                path.join(__dirname, 'views', 'index.html'),
+                'utf-8',
+                (err, content) => {
+                    if (err) {
+                        throw err
+                    }
+                    // This method signals to the server that all of the response headers and body have been sent;
+                    // that server should consider this message complete.
+                    // The method, response.end(), MUST be called on each response.
+                    res.end(content)
+                }
+            )
+        } else if (req.url === '/about') {
+            fs.readFile(
+                path.join(__dirname, 'views', 'about.html'),
+                'utf-8',
+                (err, content) => {
+                    if (err) {
+                        throw err
+                    }
 
-          // Asynchronously reads the entire contents of a file.
-          res.end(content)
+                    res.end(content)
+                }
+            )
+        } else if (req.url === '/shop') {
+            fs.readFile(
+                path.join(__dirname, 'views', 'shop.html'),
+                'utf-8',
+                (err, content) => {
+                    if (err) {
+                        throw err
+                    }
+
+                    res.end(content)
+                }
+            )
+        } else if (req.url === '/api/users') {
+            res.writeHead(200, {
+                'Content-Type': 'text/json'
+            })
+
+            const users = [
+                { name: 'Vasya', age: 20 },
+                { name: 'Elena', age: 23 }
+            ]
+
+            res.end(JSON.stringify(users))
         }
-      )
-    } else if (req.url === '/about') {
-      // Asynchronously reads the entire contents of a file.
-      fs.readFile(
-        path.join(__dirname, 'views', 'about.html'),
-        'utf-8',
-        (err, content) => {
-          if (err) {
-            throw err
-          }
+    } else if (req.method === 'POST') {
+        const body = []
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        })
 
-          // Asynchronously reads the entire contents of a file.
-          res.end(content)
-        }
-      )
+        req.on('data', data => {
+            // Copies the passed buffer data onto a new Buffer instance.
+            body.push(Buffer.from(data))
+        })
+
+        req.on('end', () => {
+            const message = body.toString().split('=')[1]
+
+            res.end(`
+                <h1>Ваше сообщение: ${message}</h1>
+            `)
+        })
     }
-  } else if (req.method === 'POST') {
-    const body = []
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
-    })
-
-    req.on('data', data => {
-      body.push(Buffer.from(data))
-    })
-
-    req.on('end', () => {
-      const message = body.toString().split('=')[1]
-
-      res.end(`
-        <h1>Ваше сообщение: ${message}</h1>
-      `)
-    })
-  }
 })
 
 server.listen(3000, () => {
-  console.log('Server is running...')
+    console.log('Server is running...')
 })
--- webserver/webserver.js
+++ webserver/webserver.js
@@ -1,30 +1,61 @@
-http = require('http')
+const http = require('http')
+const fs = require('fs')
 
-path = require('path');
+fs.readFile('img/ivkhk.jpg', function (err, data) {
+    if (err) throw err;
 
-server = http.createServer((req, res) => {
-    console.log(req.url)
+    const server = http.createServer((req, res) => {
 
-    res.write('<h1>Hello from SKTVp20!</h1>')
-    res.write('<h2>Sergey is a wonderful teacher!</h2>')
-    res.write('<h3>Ida-Virumaa Kutsehariduskeskus 2021</h3')
-    res.write('<img style="background: orange; width; 100px; height: 100px;" src="'  + path.dirname(__filename) + "/ivkhk.jpg" + '">')
+        console.log(req.url)
 
-    res.write('</div>')
+        res.write('<h1 style="background-color:MediumSeaGreen; text-align:center; text-transform: capitalize;">Hello from SKTVp20!</h1>')
+        res.write('<div style="border:1px solid gray; padding: 1em;">')
+        res.write('<h2 style="text-align: center; color: MediumSeaGreen;">Sergey is a wonderful teacher!</h2>')
+        res.write('<p style="text-indent: 50px; text-align: justify; letter-spacing: 1px;">Ida-Virumaa Kutsehariduskeskus 2021</p>')
+        res.write('<img src="data:image/jpeg;base64,')
+        res.write(Buffer.from(data).toString('base64'));
+        res.write('" style="padding: 5px; display: block; margin-left: auto; margin-right: auto; border: 1px solid #ddd; border-radius: 4px;" alt="Mountain" width="38%">');
+        res.end(`</div>`)
 
-    res.write('</div>')
+    })
 
-    res.end()
+    server.listen(3001, () => {
+        console.log('Server is running...')
+    })
+
+})
 
 
- })
 
 
- server.listen(3001, () => {
 
-    console.log('Server is running...')
 
- })
- 
- 
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
